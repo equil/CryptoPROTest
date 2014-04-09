a74957023cbaad9f7a5a5cc1f15dbc27d3e6dca0
Alexey Rogatkin <alexey.rogatkin@gmail.com>
2014-04-09 14:59:17 +0400

    Initial commit

---
+++ CryptoPROTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CryptoPROTest
+//
+//  Created by Alexey Rogatkin on 09.04.14.
+//  Copyright (c) 2014 iDecide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CryptoPROTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CryptoPROTest
+//
+//  Created by Alexey Rogatkin on 09.04.14.
+//  Copyright (c) 2014 iDecide. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
